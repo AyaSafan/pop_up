617aa893c87b4930e5fbc176b8164030ecfea992
Aya Saafan <ayasafan16@gmail.com>
2023-08-05 23:53:22 +0300

    add new file

---
+++ lib/pages/add-notification.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddNotificationPage extends StatefulWidget {
+  const AddNotificationPage({super.key});
+
+  @override
+  State<AddNotificationPage> createState() => _AddNotificationPageState();
+}
+
+class _AddNotificationPageState extends State<AddNotificationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
