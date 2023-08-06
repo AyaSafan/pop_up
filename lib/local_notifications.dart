569036db917bf0d013a3662aed573b596f229f18
Aya Saafan <ayasafan16@gmail.com>
2023-08-06 22:35:19 +0300

    add flutter_local_notifications  functions

---
+++ lib/local_notifications.dart
@@ -0,0 +1,164 @@
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:pop_up/main.dart';
+import 'package:flutter_timezone/flutter_timezone.dart';
+import 'package:timezone/timezone.dart' as tz;
+
+String currentTimezone = 'Unknown';
+
+
+initializeNotifications() async {
+
+  const AndroidInitializationSettings initializationSettingsAndroid =
+  AndroidInitializationSettings('app_icon');
+
+  const InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+  );
+
+  flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  currentTimezone = await FlutterTimezone.getLocalTimezone();
+}
+
+
+
+Future<void> onceNotification(
+    int notificationId,
+    String title,
+    DateTime dateTime,
+    String? payload,
+    String? groupKey,) async {
+
+  var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+    'PopUp','PopUp',
+    importance: Importance.max,
+    priority: Priority.high,
+    playSound: true,
+    groupKey: groupKey,
+  );
+  var platformChannelSpecifics = NotificationDetails(
+    android: androidPlatformChannelSpecifics,
+  );
+
+
+  var tzDateTime = tz.TZDateTime.from(dateTime, tz.getLocation(await FlutterTimezone.getLocalTimezone()),)
+      .add(const Duration(seconds: 1));
+
+  flutterLocalNotificationsPlugin.zonedSchedule(
+      notificationId, title, null, tzDateTime, platformChannelSpecifics,
+      payload: payload,
+      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+      uiLocalNotificationDateInterpretation:
+      UILocalNotificationDateInterpretation.absoluteTime
+  );
+}
+
+Future<void> dailyNotification(
+    int notificationId,
+    String title,
+    DateTime dateTime,
+    String? payload,
+    String? groupKey,) async {
+
+  var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+    'PopUp','PopUp',
+    importance: Importance.max,
+    priority: Priority.high,
+    playSound: true,
+    groupKey: groupKey,
+  );
+  var platformChannelSpecifics = NotificationDetails(
+    android: androidPlatformChannelSpecifics,
+  );
+
+  var tzDateTime = tz.TZDateTime.from(dateTime, tz.getLocation(await FlutterTimezone.getLocalTimezone()),)
+      .add(const Duration(seconds: 1));
+
+  flutterLocalNotificationsPlugin.zonedSchedule(
+      notificationId, title, null, tzDateTime, platformChannelSpecifics,
+      payload: payload,
+      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+      uiLocalNotificationDateInterpretation:
+      UILocalNotificationDateInterpretation.absoluteTime,
+      matchDateTimeComponents: DateTimeComponents.time
+  );
+}
+
+
+Future<void> weeklyNotification(
+    int notificationId,
+    String title,
+    DateTime dateTime,
+    String? payload,
+    String? groupKey,) async {
+
+  var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+    'PopUp','PopUp',
+    importance: Importance.max,
+    priority: Priority.high,
+    playSound: true,
+    groupKey: groupKey,
+  );
+  var platformChannelSpecifics = NotificationDetails(
+    android: androidPlatformChannelSpecifics,
+  );
+
+  var tzDateTime = tz.TZDateTime.from(dateTime, tz.getLocation(await FlutterTimezone.getLocalTimezone()),)
+      .add(const Duration(seconds: 1));
+
+  flutterLocalNotificationsPlugin.zonedSchedule(
+      notificationId, title, null, tzDateTime, platformChannelSpecifics,
+      payload: payload,
+      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+      uiLocalNotificationDateInterpretation:
+      UILocalNotificationDateInterpretation.absoluteTime,
+      matchDateTimeComponents: DateTimeComponents.dayOfWeekAndTime
+  );
+}
+
+Future<void> yearlyNotification(
+    int notificationId,
+    String title,
+    DateTime dateTime,
+    String? payload,
+    String? groupKey,) async {
+
+  var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+    'PopUp','PopUp',
+    importance: Importance.max,
+    priority: Priority.high,
+    playSound: true,
+    groupKey: groupKey,
+  );
+  var platformChannelSpecifics = NotificationDetails(
+    android: androidPlatformChannelSpecifics,
+  );
+
+  var tzDateTime = tz.TZDateTime.from(dateTime, tz.getLocation(await FlutterTimezone.getLocalTimezone()),)
+      .add(const Duration(seconds: 1));
+
+  flutterLocalNotificationsPlugin.zonedSchedule(
+      notificationId, title, null, tzDateTime, platformChannelSpecifics,
+      payload: payload,
+      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+      uiLocalNotificationDateInterpretation:
+      UILocalNotificationDateInterpretation.absoluteTime,
+      matchDateTimeComponents: DateTimeComponents.dateAndTime
+  );
+}
+
+
+Future<void> cancelNotification(int id) async {
+  await flutterLocalNotificationsPlugin.cancel(id);
+}
+
+void requestPermissions() {
+  flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+      IOSFlutterLocalNotificationsPlugin>()
+      ?.requestPermissions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,27 @@ import 'package:pop_up/pages/add-notification.dart';
 import 'package:pop_up/pages/home.dart';
 import 'package:pop_up/theme.dart';
 
-void main() {
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_timezone/flutter_timezone.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+FlutterLocalNotificationsPlugin();
+
+Future<void> _configureLocalTimeZone() async {
+  tz.initializeTimeZones();
+  final String? timeZoneName = await FlutterTimezone.getLocalTimezone();
+  tz.setLocalLocation(tz.getLocation(timeZoneName!));
+}
+
+Future<void> main() async{
+
+  // needed if you intend to initialize in the `main` function
+  WidgetsFlutterBinding.ensureInitialized();
+  await _configureLocalTimeZone();
+
   runApp(const MyApp());
 }
 
